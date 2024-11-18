ad47f652b14ce5c141fcd51c5d8140e6faf8e1a6
GenmaHimmuro <t.suleimanov2@gmail.com>
2024-11-18 14:38:14 +0500

    урок 6

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import urwid
 
+
 def is_very_long(password):
     return len(password) > 12
+
 
 def has_digit(password):
     return any(n.isdigit() for n in password)
 
+
 def has_letters(password):
     return any(n.isalpha() for n in password)
+
 
 def has_upper_letters(password):
     return any(n.isupper() for n in password)
 
+
 def has_lower_letters(password):
     return any(n.islower() for n in password)
 
+
 def has_symbols(password):
     return any(not n.isalnum() for n in password)
+
 
 def score(edit, new_password):
     score_password = 0
@@ -30,8 +37,9 @@ def score(edit, new_password):
     ]
     for correct in all_functions:
         if correct(new_password):
-            score_password += 2
+            score_password+=2
     score_text.set_text(f'Рейтинг пароля: {score_password}')
+
 
 def password():
     global score_text
@@ -41,6 +49,7 @@ def password():
     menu = urwid.Filler(menu, valign='top')
     urwid.connect_signal(ask, 'change', score)
     urwid.MainLoop(menu).run()
+
 
 if __name__ == '__main__':
     password()
